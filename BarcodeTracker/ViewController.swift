347bd07cf6f71ab3dcaca9dbfabb322c26a19da1
eldare <eldar@healthy.io>
2020-08-07 20:04:17 +0300

    ViewController - lifesycle and session setup

--- BarcodeTracker/ViewController.swift
+++ BarcodeTracker/ViewController.swift
@@ -6,4 +6,68 @@
 //  Copyright © 2020 none. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var sceneView: ARSCNView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupDebug()
+        setupScene()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setupAndStartARSession()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        puaseARSession()
+    }
+}
+
+extension ViewController {
+    private func setupDebug() {
+        sceneView.debugOptions = [
+            .showFeaturePoints,
+            .showWireframe,
+            .showWorldOrigin,
+        ]
+    }
+
+    private func setupScene() {
+        sceneView.session.delegate = self
+        sceneView.scene = SCNScene()
+    }
+
+    private func setupAndStartARSession() {
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = [.vertical]  // TODO: incomplete - *ELDAR* - explain assumption for vertical only
+        sceneView.session.run(configuration)
+    }
+
+    private func puaseARSession() {  // TODO: incomplete - *ELDAR* - is the start after pause written correctly?
+        sceneView.session.pause()
+    }
+}
+
+extension ViewController {
+    private func startBarcodeDetection() {
+        // TODO: incomplete - *ELDAR* -
+    }
+}
+
+extension ViewController: ARSessionDelegate {
+    func session(_ session: ARSession, didUpdate frame: ARFrame) {
+        // TODO: incomplete - *ELDAR* -
+    }
+}
